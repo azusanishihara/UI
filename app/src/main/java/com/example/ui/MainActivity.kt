a671db00a258e0311d7bbc9cfae508fcb82e85e0
azusa <azusa.nishihara@sbworks.jp>
2020-03-15 12:13:41 +0900

    Leson4課題提出用

---
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.ui
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button3.setOnClickListener(this)
+
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        if( 2 <= hour && hour < 10 ){
+            textView.text = "おはよう"
+        }else if (10 <= hour && hour < 18){
+            textView.text = "こんにちは"
+        }else{
+            textView.text = "こんばんは"
+        }
+    }
+    
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.button3 -> showTimePickerDialog()
+        }
+    }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener {view, hour, minute ->
+                Log.d("UI_PARTS", "$hour:$minute")
+            },
+            13, 0, true)
+        timePickerDialog.show()
+    }
+
+}
